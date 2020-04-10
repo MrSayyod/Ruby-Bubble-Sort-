fd5454aed0f9f2ff6aa56711ce0b88c49188b70d
MrSayyod <ssayyod@gmail.com>
2020-04-10 14:16:25 +0500

    added def methods

---
+++ bubble.rb
@@ -0,0 +1,9 @@
+def bubble_sort array
+  if array[i]>array[i+1]
+    array[i], array[i+1] = array[i+1], array[i]
+    end
+end
+def bubble_sort_by array
+end
+
+puts bubble_sort([1, 5, 9, 654, 23, 7, 3])
