dbf03cdb918753ad725da8c3abaff94c6dff57c3
MrSayyod <ssayyod@gmail.com>
2020-04-10 18:08:48 +0500

    fixed linter errors

--- bubble.rb
+++ bubble.rb
@@ -1,14 +1,14 @@
 def bubble_sort(array)
   switch = true
   n = array.length
-  while switch 
+  while switch
     switch = false
-    (n-1).times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         switch = true
       end
-      end
+    end
   end
   array
 end
@@ -16,23 +16,22 @@ end
 print bubble_sort([1, 5, 9, 654, 23, 7, 3])
 puts ''
 
-
 def bubble_sort_by(array)
   switch = true
   n = array.length
-  while switch 
+  while switch
     switch = false
-    (n-1).times do |i|
+    (n - 1).times do |i|
       if yield(array[i], array[i + 1]) > 0
-        array[i], array[i+1] = array[i+1], array[i]
+        array[i], array[i + 1] = array[i + 1], array[i]
         switch = true
       end
-      end
+    end
   end
   array
 end
 
-sort_by = bubble_sort_by(['hi', 'hello', 'hey', 'halloween', 'honeymoon']) do |left,right|
+sort_by = bubble_sort_by(%w[hi hello hey halloween honeymoon]) do |left, right|
   left.length - right.length
 end
 
