bffbd6c81485367d939717352dfd0d681834254b
Hermela-Fikre <hermelafikre3@gmail.com>
2020-04-10 13:52:52 +0300

    Solved bubble sort part

--- bubble.rb
+++ bubble.rb
@@ -1,9 +1,18 @@
-def bubble_sort array
-  if array[i]>array[i+1]
-    array[i], array[i+1] = array[i+1], array[i]
-    end
-end
-def bubble_sort_by array
+def bubble_sort(array)
+  switch = true
+  n = array.length
+  while switch 
+    switch = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        switch = true
+      end
+      end
+  end
+  array
 end
 
 puts bubble_sort([1, 5, 9, 654, 23, 7, 3])
+
+
